fd0dc185208911518b93552abf158c499bf2b18b
wenjuin11 <102800631+wenjuin11@users.noreply.github.com>
2022-04-02 23:24:20 +0800

    Delete config.py

--- config.py
+++
@@ -1,6 +0,0 @@
-customhost = "employee.cspnc5ouklrd.us-east-1.rds.amazonaws.com"
-customuser = "aws_user"
-custompass = "Bait3273"
-customdb = "employee"
-custombucket = "lowchoonkeat-employee"
-customregion = "us-east-1"
